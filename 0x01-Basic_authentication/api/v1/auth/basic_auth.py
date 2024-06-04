4c8206b25843ca4b972416f935241953cf2372ce
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-05 00:46:24 +0100

    new tasks

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic_auth class
+"""
+
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """the BasicAuth class"""
+    pass
