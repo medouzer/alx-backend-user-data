92f0133637c805687f674d061763e53241225a4e
mouzerzo <mouzerzo@e2r2p2.1337.ma>
2024-06-05 21:42:57 +0100

    new task

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """the class Auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method require_auth"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """public method authorization_header"""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method current_user"""
+        return None
