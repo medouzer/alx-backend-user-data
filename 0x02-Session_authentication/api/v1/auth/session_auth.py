fdb7a1ff5f31a97d686991ccf16201533ab71ee8
mouzerzo <mouzerzo@e2r2p2.1337.ma>
2024-06-05 23:43:21 +0100

    new things

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Empty session class
+"""
+
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+import uuid 
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """create_session method"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user_id_for_session_id"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        user_id = self.__class__.user_id_by_session_id.get(session_id, None)
+        return user_id
