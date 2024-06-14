8861348870405f640d2b168f6c63c92db863097d
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-14 01:32:08 +0100

    fix check error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,12 +32,13 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """create user"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
         try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            session = self._session
             session.add(new_user)
             session.commit()
         except Exception as e:
             session.rollback()
             print(f"Error adding user to the database: {e}")
+            raise
         return new_user
